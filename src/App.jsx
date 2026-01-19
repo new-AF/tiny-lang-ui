2625ae70067d0d7bd4e61217153c85f0a11d5f30
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2026-01-19 10:35:38 +0100

    add textarea

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,28 @@ export const App = () => {
                     Tiny Lang
                 </h1>
             </header>
-            <main className={mergeClassNames("px-(--spacing-sm)")}>test1</main>
+            <main className={mergeClassNames("px-(--spacing-sm)")}>
+                <textarea
+                    type="text"
+                    placeholder="Type your program here..."
+                    className={mergeClassNames(
+                        "block",
+                        "w-full",
+                        "px-3",
+                        "py-2",
+                        "border",
+                        "border-slate-300",
+                        "rounded-md",
+                        "shadow-sm",
+                        "placeholder:text-slate-400",
+                        "focus:outline-none",
+                        "focus:border-blue-500",
+                        "focus:ring-blue-500",
+                        "sm:text-sm",
+                        "resize-y",
+                    )}
+                />
+            </main>
             <footer
                 className={mergeClassNames(
                     "text-center",
